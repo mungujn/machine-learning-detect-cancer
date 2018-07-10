c3e73aed739ef12504cf33439ddfda07fede031e
Mungujakisa Nickson <nicksonmjk@gmail.com>
2018-07-10 10:53:37 +0300

    code and models as per my dissertation

--- functions/__init__.py
+++
@@ -1,1 +0,0 @@
-from functions.timing import *
--- scripts.py
+++
@@ -1,5 +0,0 @@
-from functions.transform_images import performTransformationSetWithBlurAndSharpen, \
-    resize, getFileAddresses, convertToRGB, toNpArray
-
-dir = "J:\\final year project\\code and models\\data\\original\\testing\\combined\\"
-
